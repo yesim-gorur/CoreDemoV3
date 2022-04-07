0cfba2636409543d1cfc1952af7e2994a733b1d5
yesim gorur <yesim.gorur@bahcesehir.k12.tr>
2022-04-07 12:46:19 +0300

    20. ders sonu

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //constructer oluşturacagım ben sadece EFCategoryRepositoryi kullanacagım çünkü
+        //bu hem genericrepositoryi tutuyor hemde Icategorydal'ı tutuyor
+        EfCategoryRepository efCategoryRepository;
+        public CategoryManager()
+        {
+            efCategoryRepository=new EfCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            efCategoryRepository.Insert(category);
+
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            efCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            efCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+           return efCategoryRepository.GetListAll();
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class
+    {
+        //generic hale getiriyoruz
+
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+
+
+
+    }
+}
