d43d2bef8c401634c253a24c58286e39643e7808
yesim gorur <yesim.gorur@bahcesehir.k12.tr>
2022-04-08 14:43:43 +0300

    24.ders ortası

--- CoreDemoV3/Controllers/BlogController.cs
+++ CoreDemoV3/Controllers/BlogController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemoV3.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
             //index blogların listelendiği sayfa
-            return View();
+
+            var values=bm.GetList();
+
+            return View(values);
         }
     }
 }
